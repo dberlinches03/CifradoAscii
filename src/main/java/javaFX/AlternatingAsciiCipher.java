f0c6758f65af663e17fbf472553a911656917453
dberl <dberlinches2003@gmail.com>
2026-02-04 17:20:30 +0100

    Implementado funciones de cifrado y descifrado

--- src/main/java/javaFX/AlternatingAsciiCipher.java
+++ src/main/java/javaFX/AlternatingAsciiCipher.java
@@ -1,7 +1,40 @@
 package javaFX;
 
 public class AlternatingAsciiCipher {
-    public static String cifrar(String mensaje, int clave) {
+    public String cifrar(String mensaje, int clave) {
+        StringBuilder resultado = new StringBuilder();
 
+        for (int i = 0; i < mensaje.length(); i++) {
+            char c = mensaje.charAt(i);
+            int ascii = (int) c;
+
+            if (i % 2 == 0) {
+                // Posición par -> sumar clave
+                ascii += clave;
+            } else {
+                // Posicion impar -> restar clave
+                ascii -= clave;
+            }
+            resultado.append((char) ascii);
+        }
+        return resultado.toString();
+    }
+
+    public String descrifrar(String mensajeCifrado, int clave) {
+        StringBuilder resultado = new StringBuilder();
+        for (int i = 0; i < mensajeCifrado.length(); i++) {
+            char c = mensajeCifrado.charAt(i);
+            int ascii = (int) c;
+
+            if (i % 2 == 0) {
+                // Posición par -> restar clave
+                ascii -= clave;
+            } else {
+                // Posición impar -> sumar clave
+                ascii += clave;
+            }
+            resultado.append((char) ascii);
+        }
+        return resultado.toString();
     }
 }
--- src/main/java/javaFX/Main.java
+++ src/main/java/javaFX/Main.java
@@ -2,6 +2,14 @@ package javaFX;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        AlternatingAsciiCipher cipher = new AlternatingAsciiCipher();
+
+        String mensajeOriginal = "HELLO";
+        int clave = 3;
+        String cifrado = cipher.cifrar(mensajeOriginal, clave);
+        System.out.println("Mensaje cifrado: " + cifrado);
+
+        String descifrado = cipher.descrifrar(cifrado, clave);
+        System.out.println("Mensaje descifrado: " + descifrado);
     }
 }
